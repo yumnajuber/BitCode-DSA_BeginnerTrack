19688dea0f19f843fdba290159e32c09180be25f
Yumna Juber <yumnajuber@gmail.com>
2026-06-26 14:38:16 +0300

    Day 1 Data Types and Division practice

---
+++ DAY1-DataTypes/datatypes.py
@@ -0,0 +1,32 @@
+# Integer represents whole numbers
+x = 10
+# Float represents decimals
+y = 3.5
+# String represents text
+name = "BitCode"
+# Boolean represents a True/False value
+is_learning = True
+# Printing types, this tells you what kind of data something is
+print(type(x))
+print(type(y))
+print(type(name))
+print(type(is_learning))
+# Converting values
+print ("10")
+print (int("10"))
+print (float("10"))
+print (str(10))
+# Performing operations
+print ("5"+"2")
+print (5+2)
+print ("BitCode"*3)
+# Division Task example
+x = 5
+y = 2
+print(x // y)
+print(x / y)
+# HackerRank Division Task Solution
+a = int(input())
+b = int(input())
+print(a // b)
+print(a / b)
